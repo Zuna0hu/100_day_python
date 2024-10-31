0eb368506f10d1226102615fc8ebd70686a67c4d
Zuna0hu <mythhuza@gmail.com>
2024-10-30 23:07:30 -0400

    Initialize main.py for turtle test
    Create and initialize main.py to import the Turtle class and create an
    object for testing. This setup provides a base to explore Turtle
    graphics and behavior.

---
+++ Day16/main.py
@@ -0,0 +1,14 @@
+# need to install tkinter
+import turtle
+
+# turtle is a module
+# Turtle is a class
+
+# create an object
+timmy = turtle.Turtle()
+
+# also you can use
+# from turtle import Turtle
+# timmy = Turtle()
+
+print(timmy)
