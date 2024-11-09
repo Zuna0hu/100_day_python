c51ff8563992549da5fc777e5e78d06738804796
Zuna0hu <mythhuza@gmail.com>
2024-11-08 23:41:00 -0500

    Set PrettyTable alignment to left with comments
    Set the alignment of the PrettyTable object to left to demonstrate
    modifying object attributes. Add empty lines and comments for
    readability.

--- Day16/my_prettytable.py
+++ Day16/my_prettytable.py
@@ -1,6 +1,14 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
+
+# add columns
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# change the alignment to left 
+table.align = "l"
+# in PrettyTable, the alignment setting is stored separately for each column rather than for the entire table
+# if set table.align = "l" before adding any columns, there are no columns yet for the setting to apply to
+
 print(table)
