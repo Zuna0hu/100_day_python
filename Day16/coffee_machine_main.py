e47d5380c509659cd8d39eb246f8a73d5ee6822e
Zuna0hu <mythhuza@gmail.com>
2024-11-11 16:46:27 -0500

    Add coffee-making and report update functions
    Implement functions in coffee_machine_main.py using methods from
    coffee_maker.py and menu.py to check ingredient availability, display
    appropriate messages, and update the report. This enhances the coffee
    machine functionality.

--- Day16/coffee_machine_main.py
+++ Day16/coffee_machine_main.py
@@ -14,18 +14,27 @@ def coffee_machine():
         # Check the user's input and dispense the drink
         if choice == "espresso":
             print("Dispensing espresso...")
+            if (coffee_maker.is_resource_sufficient(menu.menu[1])):
+                coffee_maker.make_coffee(menu.menu[1])
         elif choice == "latte":
             print("Dispensing latte...")
+            if (coffee_maker.is_resource_sufficient(menu.menu[0])):
+                coffee_maker.make_coffee(menu.menu[0])
         elif choice == "cappuccino":
             print("Dispensing cappuccino...")
+            if (coffee_maker.is_resource_sufficient(menu.menu[2])):
+                coffee_maker.make_coffee(menu.menu[2])
         elif choice == "off":
             print("Goodbye!")
             break
+        elif choice == "report":
+            coffee_maker.report()
         else:
             print("Invalid option. Please choose espresso, latte, or cappuccino.")
         
         # Show prompt again after drink is dispensed
         print("Ready for the next customer!\n")
+
 
 # Run the coffee machine
 coffee_machine()
