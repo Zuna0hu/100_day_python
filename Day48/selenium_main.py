8933c39f075a2638ae18c6006d795d767b1b0dcb
Zuna0hu <mythhuza@gmail.com>
2024-11-13 21:37:32 -0500

    Add comment on closing and quitting in Selenium
    Add a comment showing how to use close() and quit() methods in Selenium.

--- Day48/selenium_main.py
+++ Day48/selenium_main.py
@@ -7,3 +7,7 @@ chrome_options.add_experimental_option("detach", True)
 # create a driver for Chrome
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.amazon.com")
+
+# to close the browser automatically
+# driver.close() # close the tab only
+# driver.quit() # quit the entire browser 
