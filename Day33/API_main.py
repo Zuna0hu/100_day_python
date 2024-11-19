7377364e6b6ef194033b1c2506d38a28a6a3b5da
Zuna0hu <mythhuza@gmail.com>
2024-11-18 23:17:38 -0500

    Retrieve real data in API_main.py
    Use the requests library to pull JSON data.
    Add comments explaining how to use the JSON data as a Python dictionary.

--- Day33/API_main.py
+++ Day33/API_main.py
@@ -2,4 +2,24 @@ import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 
-print(response)
+# print(response)
+print(response.status_code)
+
+# In Python, raise Exception is used to explicitly raise an exception, 
+# which interrupts the normal flow of a program and triggers the error-handling mechanism.
+# if response.status_code == 404:
+#     raise Exception("That resource does not exist.") # raise a specifc exception
+# elif response.status_code == 401:
+#     raise Exception("You are not authorized to access this data.")
+
+# but instead of doing it manually
+# we can use the request module
+response.raise_for_status()
+
+# to get acutal data
+data = response.json()
+
+# we can use it like python dictionary
+# iss_postion = data["iss_postion"]
+# longitude = data["iss_position"]["longitude"]
+print(data)
