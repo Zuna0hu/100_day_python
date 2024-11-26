0724fa6add868d4383bf560e37775c02c86754e4
Zuna0hu <mythhuza@gmail.com>
2024-11-25 23:55:33 -0500

    Encapsulate the code into functions
    Encapsulate the code into functions of displaying the sunrise and sunset
    times in local time of Montreal, and tell if it is dark now.

--- Day33/sunrise_sunset.py
+++ Day33/sunrise_sunset.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 45.501690
+MY_LAT = 45.501690 # Montreal
 MY_LONG = -73.567253
 
 my_params = {
@@ -38,14 +38,60 @@ sunset_min = sunset.split("T")[1].split(":")[1]
 # print(f"sunset time in Montreal(UTC): {sunset_hour}h{sunset_min}")
 
 # Get the current date
+
 now = datetime.now()
 
+
+def print_time_in_montreal():
 # approximately determine if it's DST in Montreal
-if (now.month > 3 and now.month < 11) or (now.month == 3 and now.day >= 8) or (now.month == 11 and now.day < 7): # not accurate
-    # minus 4
-    print(f"sunrise time in Montreal(local): {(int(sunrise_hour)+20)%24}h{sunrise_min}")
-    print(f"sunset time in Montreal(local): {(int(sunset_hour)+20)%24}h{sunset_min}")
-else:
-    # minus 5
-    print(f"sunrise time in Montreal(local): {(int(sunrise_hour)+19)%24}h{sunrise_min}")
-    print(f"sunset time in Montreal(local): {(int(sunset_hour)+19)%24}h{sunset_min}")
+    if (now.month > 3 and now.month < 11) or (now.month == 3 and now.day >= 8) or (now.month == 11 and now.day < 7): # not accurate
+        # minus 4
+        print(f"sunrise time in Montreal(local): {(int(sunrise_hour)+20)%24}h{sunrise_min}")
+        print(f"sunset time in Montreal(local): {(int(sunset_hour)+20)%24}h{sunset_min}")
+    else:
+        # minus 5
+        print(f"sunrise time in Montreal(local): {(int(sunrise_hour)+19)%24}h{sunrise_min}")
+        print(f"sunset time in Montreal(local): {(int(sunset_hour)+19)%24}h{sunset_min}")
+
+def is_dark_in_montreal():
+# approximately determine if it's DST in Montreal
+    if (now.month > 3 and now.month < 11) or (now.month == 3 and now.day >= 8) or (now.month == 11 and now.day < 7): # not accurate
+        # minus 4
+        sunrise_hour_montreal = (int(sunrise_hour)+20)%24
+        sunrise_min_montreal = int(sunrise_min)
+        sunrise_in_min = sunrise_hour_montreal*24 + sunrise_min_montreal # use total min to compare time in montreal
+
+        sunset_hour_montreal = (int(sunset_hour)+20)%24
+        sunset_min_montreal = int(sunset_min)
+        sunset_in_min = sunset_hour_montreal*24 + sunset_min_montreal
+
+        now_in_min = now.hour*24 + now.minute
+        if now_in_min >= sunset_in_min or now_in_min <= sunrise_in_min:
+            return True
+        else:
+            return False
+
+    else:
+        # minus 5
+        sunrise_hour_montreal = (int(sunrise_hour)+19)%24
+        sunrise_min_montreal = int(sunrise_min)
+        sunrise_in_min = sunrise_hour_montreal*24 + sunrise_min_montreal # use total min to compare time in montreal
+
+        sunset_hour_montreal = (int(sunset_hour)+19)%24
+        sunset_min_montreal = int(sunset_min)
+        sunset_in_min = sunset_hour_montreal*24 + sunset_min_montreal
+
+        now_in_min = now.hour*24 + now.minute
+        if now_in_min >= sunset_in_min or now_in_min <= sunrise_in_min:
+            return True
+        else:
+            return False
+        
+
+print(now.hour)
+print(type(now.hour))
+print(now.minute)
+print(type(now.minute))
+
+print_time_in_montreal()
+print(is_dark_in_montreal())
