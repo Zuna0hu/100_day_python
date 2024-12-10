45a4b47383ee2fd8d654ec47d0bf3835db49a324
Zuna0hu <mythhuza@gmail.com>
2024-12-10 18:28:31 -0500

    Initialize quiz_main.py
    Create `User` class and two objects of that class.

---
+++ Day17/quiz_main.py
@@ -0,0 +1,15 @@
+# create a class
+
+class User: # Pascal Case
+    pass
+
+user_1 = User()
+user_1.id = "001" # create a variable
+user_1.username = "angela" # create another variable
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "jacques" # the starting information are repeated
+
+print(user_1.id)
+print(user_1.username)
