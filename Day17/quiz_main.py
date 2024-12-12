23f10e4827bb441bb6f01890b39f9a478e20f353
Zuna0hu <mythhuza@gmail.com>
2024-12-12 15:53:58 -0500

    Add init function and follow method to User class

--- Day17/quiz_main.py
+++ Day17/quiz_main.py
@@ -1,15 +1,34 @@
 # create a class
 
 class User: # Pascal Case
-    pass
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
 
-user_1 = User()
-user_1.id = "001" # create a variable
-user_1.username = "angela" # create another variable
+        self.followers = 0
+        self.following = 0
 
-user_2 = User()
-user_2.id = "002"
-user_2.username = "jacques" # the starting information are repeated
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
-print(user_1.id)
-print(user_1.username)
+# user_1 = User()
+# user_1.id = "001" # create a variable
+# user_1.username = "angela" # create another variable
+
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "jacques" # the starting information are repeated
+
+# print(user_1.id)
+# print(user_1.username)
+user_1 = User("001", "angela")
+user_2 = User("002", "jack")
+
+user_1.follow(user_2)
+
+print(user_1.following)
+print(user_1.followers)
+
+print(user_2.following)
+print(user_2.followers)
