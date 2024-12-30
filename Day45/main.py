80ea2f7367776049d2d70bfeb243c93e15177a39
Zuna0hu <mythhuza@gmail.com>
2024-12-29 22:40:41 -0500

    Initialize main.py
    Initialize main.py so that it reads the contents of website.html.

---
+++ Day45/main.py
@@ -0,0 +1,5 @@
+# main.py
+with open("website.html", "r") as file:
+    html_content = file.read()
+    # Now you can work with the html_content variable
+    print(html_content)  # Example of working with the content
