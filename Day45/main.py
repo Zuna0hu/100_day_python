a9485f1cad11372bd6e2c1611d1a045c3679aaf6
Zuna0hu <mythhuza@gmail.com>
2024-12-31 23:48:52 -0500

    Add tests of parsing html doc using beautifulsoup

--- Day45/main.py
+++ Day45/main.py
@@ -1,5 +1,13 @@
 # main.py
+from bs4 import BeautifulSoup
+
 with open("website.html", "r") as file:
     html_content = file.read()
     # Now you can work with the html_content variable
-    print(html_content)  # Example of working with the content
+    # print(html_content)  # Example of working with the content
+
+soup = BeautifulSoup(html_content, "html.parser")
+
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
