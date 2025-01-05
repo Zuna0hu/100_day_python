9539781cfde483e140e62c7eb31c3f5dd45a9cda
Zuna0hu <mythhuza@gmail.com>
2025-01-05 00:55:57 -0500

    Add examples of using bs4 to find elements

--- Day45/main.py
+++ Day45/main.py
@@ -11,3 +11,13 @@ soup = BeautifulSoup(html_content, "html.parser")
 print(soup.title)
 print(soup.title.name)
 print(soup.title.string)
+
+all_anchor_tags = soup.find_all(name="a")
+
+# print(all_anchor_tags)
+
+# for tag in all_anchor_tags:
+#     print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading.getText())
